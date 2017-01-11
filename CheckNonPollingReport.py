a1dd3bef816d08354e381ab815866be04bab25a4
Zeerti <skarrj@gmail.com>
2017-01-11 13:13:31 -0700

    Fixed Script Breaking Bug
    Reading in the Excel file was appending  .0 to the end of each site ID.
    Script was compairing XXX.0 to XXX and always returned false. Had to
    convert to an integer to resolve issue.

--- CheckNonPollingReport.py
+++ CheckNonPollingReport.py
@@ -31,7 +31,7 @@ def _get_current_site_ID_with_open_case():
 		for line in currentSites:
 			matchObject = re.match(patternSiteID, line) #Find the BR store #
 			if matchObject:
-				list_site_id_with_case.append(matchObject.group(0))
+				list_site_id_with_case.append(int(matchObject.group(0)))
 				matchObject = None
 	print("Parsed Pre-Existing Sites listed in file {} \n".format(fileName))
 
@@ -45,31 +45,46 @@ def _get_updated_site_ID_from_daily_report(): #Open Daily Excel File and pull BR
 	sheetList = currentWorkbook.get_sheet_names() #get list of sheets
 	currentSheet = currentWorkbook.get_sheet_by_name(sheetList[0]) #Set active sheet to first in list
 
-	columnA = currentSheet['A'] #Return all values in 
+	columnA = currentSheet['A'] #Return all values in active sheet's column A
+	
 	for column in columnA:
-		columnValue = column.value
-		
-		if columnValue != None:
-			matchObject = re.match(patternSiteID, str(columnValue)) #Find the BR store number, convert to string for regular expression scanning
 
-		if matchObject:
-			list_site_id_daily_poll.append(columnValue) #Match found, save it.
+		columnValue = column.value
+				
+		if columnValue is not None:
+			try:
+				columnValue = int(columnValue) #Excel Doc is reading in with trailing zeros. This removes those zeros.
+	
+				if columnValue is not None:
+					matchObject = re.match(patternSiteID, str(columnValue)) #Find the BR store number, convert to string for regular expression scanning
+					
+					if matchObject:
+						list_site_id_daily_poll.append(columnValue) #Match found, save it.
+						#print("Found Match: {}".format(columnValue)
+	
+			except ValueError:
+				print("Excel: '{}'  Failed to convert Type {}".format(columnValue, type(columnValue)))
+		
+		
 
 	print("Parsed Updates Sites listed in Excel file {} \n".format(fileName))
 	
 def _compare_current_and_daily_lists(): #Compare lists to find BR Store number missing from list. These can be closed.
-	
-	###########print("{} \n\n\n".format(list_site_id_with_case))
-	###########print(list_site_id_daily_poll)
+	print(len(list_site_id_with_case))
+	print(len(list_site_id_daily_poll))
 	if(len(list_site_id_with_case) >= len(list_site_id_daily_poll)):
 		#check new daily excel list against pre-existing list
 		for i in range(len(list_site_id_with_case), 0, -1):
+			#print("Type1: {}       Type2: {}".format(type(list_site_id_with_case[i-1]), type(list_site_id_daily_poll[i-2])))
+			#print("Type1: {}              Type2: {}".format(list_site_id_with_case[i-1], list_site_id_daily_poll[i-2]))
 			if list_site_id_with_case[i-1] not in list_site_id_daily_poll:
 				list_sites_missing.append(list_site_id_with_case[i-1])
 	else:
 		#check pre-existing list against daily excel
 		for i in range(len(list_site_id_daily_poll), 0, -1):
-			if str(list_site_id_daily_poll[i-1]) not in list_site_id_with_case:
+			#print("Type1: {}     Type2: {}".format(type(list_site_id_with_case[i-(len(list_site_id_daily_poll)-1)]), type(list_site_id_daily_poll[i-1])))
+			#print("Type1: {}            Type2: {}".format(list_site_id_with_case[i-(len(list_site_id_daily_poll)-1)], list_site_id_daily_poll[i-1]))
+			if int(list_site_id_daily_poll[i-1]) not in list_site_id_with_case:
 				list_sites_missing.append(list_site_id_daily_poll[i-1])
 
 
